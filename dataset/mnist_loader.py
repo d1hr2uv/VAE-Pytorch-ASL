0cd1a3147a5a1cd46452cc446f3b9e9f0b5c8561
Dhruba Patra <153985618+d1hr2uv@users.noreply.github.com>
2024-06-26 05:42:37 +0530

    Update mnist_loader.py

--- dataset/mnist_loader.py
+++ dataset/mnist_loader.py
@@ -7,25 +7,22 @@ from torch.utils.data.dataset import Dataset
 from torch.utils.data.dataloader import DataLoader
 
 
-r"""
-Simple Dataloader for mnist.
-This assumes the images are already extracted from csv.
-For extracting images from csv one can use utils/extract_mnist_images
-"""
-class MnistDataset(Dataset):
-    def __init__(self, split, im_path, im_ext='png'):
+class ASLDataset(Dataset):
+    def __init__(self, split, im_path, im_ext='jpg'):
         self.split = split
         self.im_ext = im_ext
         self.images, self.labels = self.load_images(im_path)
         
     def load_images(self, im_path):
-        assert os.path.exists(im_path), "images path {} does not exist".format(im_path)
+        assert os.path.exists(im_path), "Images path {} does not exist".format(im_path)
         ims = []
         labels = []
         for d_name in tqdm(os.listdir(im_path)):
-            for fname in glob.glob(os.path.join(im_path, d_name, '*.{}'.format(self.im_ext))):
-                ims.append(fname)
-                labels.append(int(d_name))
+            dir_path = os.path.join(im_path, d_name)
+            if os.path.isdir(dir_path):
+                for fname in glob.glob(os.path.join(dir_path, '*.{}'.format(self.im_ext))):
+                    ims.append(fname)
+                    labels.append(d_name)  # Use directory name as label
         print('Found {} images for split {}'.format(len(ims), self.split))
         return ims, labels
 
@@ -33,23 +30,19 @@ class MnistDataset(Dataset):
         return len(self.images)
     
     def __getitem__(self, index):
-        im = cv2.imread(self.images[index], 0)
+        im = cv2.imread(self.images[index], cv2.IMREAD_GRAYSCALE)
         label = self.labels[index]
         # Convert to 0 to 255 into -1 to 1
-        im = 2*(im / 255) - 1
-        # Convert H,W,C into 1,C,H,W
-        im_tensor = torch.from_numpy(im)[None,:]
-        return im_tensor, torch.as_tensor(label)
-        
+        im = 2 * (im / 255) - 1
+        # Convert H, W into 1, H, W
+        im_tensor = torch.from_numpy(im).unsqueeze(0)
+        return im_tensor, torch.tensor(ord(label) - ord('A'))  # Convert label to a numerical value
 
 
 if __name__ == '__main__':
-    mnist = MnistDataset('test', 'data/test/images')
-    mnist_loader = DataLoader(mnist, batch_size=16, shuffle=True, num_workers=0)
-    for im, label in mnist_loader:
-        print('Image dimension', im.shape)
-        print('Label dimension: {}'.format(label.shape))
+    asl_dataset = ASLDataset('test', 'path_to_your_asl_dataset')
+    asl_loader = DataLoader(asl_dataset, batch_size=16, shuffle=True, num_workers=0)
+    for im, label in asl_loader:
+        print('Image dimension:', im.shape)
+        print('Label:', label)
         break
-    
-    
-    
